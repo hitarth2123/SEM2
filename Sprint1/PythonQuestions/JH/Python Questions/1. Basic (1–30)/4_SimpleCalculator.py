9594fa35cc3dd19ed8bb2a6986760656e6f4fc95
hitarth2123 <hitarthv2115@gmail.com>
2025-01-14 23:50:46 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/JH/Python Questions/1. Basic (1–30)/4_SimpleCalculator.py
@@ -0,0 +1,10 @@
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
+sum=a+b
+print("Sum of two numbers is :",sum)
+sub=a-b
+print("Subtraction of two numbers is :",sub)
+mul=a*b
+print("Multiplication of two numbers is :",mul)
+div=a/b
+print("Division of two numbers is :",div)
