21314f4b5ae7212a8b688631b9b4efb0fe6be03b
hitarth2123 <hitarthv2115@gmail.com>
2025-01-14 14:15:17 +0530

    Sprint1

---
+++ Sprint1/JAVA/classwork/JaggedArray.java
@@ -0,0 +1,27 @@
+
+class JaggedArray {
+    public static void main(String[] args) {
+        int two[][]= new int[4][];
+        two[0]=new int[3];
+        two[1]=new int[2];
+        two[2]=new int[1];
+        two[3]=new int[0];
+        int i,j,k=0;
+        for(i=0;i<4;i++)
+        {
+            for(j=0;j<i+1;j++)
+            {
+                two[i][j]=k;
+                k++;
+            }
+        }
+        for(i=0;i<4;i++)
+        {
+            for(j=0;j<i+1;j++)
+            {
+                System.out.print(two[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
