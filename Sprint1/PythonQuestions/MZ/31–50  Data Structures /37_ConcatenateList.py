b84116a8ba90ce108ef139d4a26c7bd52fcf203d
hitarth2123 <hitarthv2115@gmail.com>
2025-01-14 14:22:17 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/MZ/31–50  Data Structures /37_ConcatenateList.py
@@ -0,0 +1,4 @@
+list1 = list(map(int, input("Enter the first list of numbers separated by space: ").split()))
+list2 = list(map(int, input("Enter the second list of numbers separated by space: ").split()))
+concatenated_list = list1 + list2
+print("Concatenated List: ", concatenated_list)
