117a5eb1bf90ccddb5505eb410a885e5f5a740cf
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 18:21:18 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/JH/Python Questions/1. Basic (1–30)/30_PasswordMaking.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_password(length):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+"
+    return ''.join(random.choice(characters) for i in range(length))
+
+length = int(input("Enter the length of the password: "))
+password = generate_password(length)
+print("Generated Password : ", password)
+
