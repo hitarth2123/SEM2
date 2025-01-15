3c1f042e87de986e3ef320627bb2a9dbbc0bc9d6
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 19:44:58 +0530

    Sprint1

--- Sprint1/PythonQuestions/JH/Python Questions/2. Intermediate (31–60)/43_ListComprehension.py
+++ Sprint1/PythonQuestions/JH/Python Questions/2. Intermediate (31–60)/43_ListComprehension.py
@@ -1,4 +1,4 @@
 lower_limit = int(input("Enter the lower limit: "))
 upper_limit = int(input("Enter the upper limit: "))
 squares = [x**2 for x in range(lower_limit, upper_limit + 1)]
-print("\n squares value is ",squares)
+print("\nThe squares of numbers in the range from", lower_limit, "to", upper_limit, "are: ", squares)
