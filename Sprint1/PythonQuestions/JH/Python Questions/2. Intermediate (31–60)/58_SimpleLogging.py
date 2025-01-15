8c91e0751e219edf85183f133a9a3708c76f0499
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 21:15:31 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/JH/Python Questions/2. Intermediate (31–60)/58_SimpleLogging.py
@@ -0,0 +1,11 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info('\nThis is an info message')
+logging.error('\nThis is an error message')
+logging.critical('\nThis is a critical message')
+logging.warning('\nThis is a warning message')
+
+
+
+
+
