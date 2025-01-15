9f056b5dc0ff9460a4c31eda33fdec581d8fa520
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 18:16:57 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/JH/Python Questions/1. Basic (1–30)/27_ReplaceCharacter.py
@@ -0,0 +1,8 @@
+original_string = input("Enter a string: ")
+char_to_replace = input("Enter the character to replace: ")
+replacement_char = input("Enter the replacement character: ")
+new_string = original_string.replace(char_to_replace, replacement_char)
+print("New string: ", new_string)
+
+
+
