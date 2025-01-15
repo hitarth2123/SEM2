03020fc76b66871473de6dda33b62dd3673275c9
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 18:04:46 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/JH/Python Questions/1. Basic (1–30)/23_CountOccurrences.py
@@ -0,0 +1,5 @@
+lst = input("Enter a list of elements separated by space: ").split()
+x = input("Enter the element to be counted: ")
+print("The element", x, "appears", lst.count(x), "times in the list.")
+
+
