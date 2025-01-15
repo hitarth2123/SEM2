a8b53e4b69cfc0859893c46c77b5a101a4402254
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 21:38:40 +0530

    Sprint1

---
+++ Sprint1/PythonQuestions/JH/Python Questions/2. Intermediate (31–60)/60_JSONwriteAndRead.py
@@ -0,0 +1,34 @@
+
+import json
+
+def create_json_file(file_path):
+    data = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def update_json_file(file_path, key, new_value):
+    with open(file_path, 'r+') as file:
+        data = json.load(file)
+        data[key] = new_value
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+file_path = 'example.json'
+create_json_file(file_path)
+update_json_file(file_path, 'age', 31)
+
+
+with open(file_path, 'r') as file:
+    data = json.load(file)
+    print("\nUser Output:")
+    for key, value in data.items():
+        print(f"{key}: {value}")
+
+
+
+
