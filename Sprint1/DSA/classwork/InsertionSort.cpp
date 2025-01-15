81e184737797cfb9602b903e760a015f9ea7af94
hitarth2123 <hitarthv2115@gmail.com>
2025-01-15 11:45:17 +0530

    Sprint1

--- Sprint1/DSA/classwork/InsertionSort.cpp
+++ Sprint1/DSA/classwork/InsertionSort.cpp
@@ -1,39 +1,42 @@
 #include <iostream>
 using namespace std;
-    class InsertionSort {
-    public:
-        void sort(int arr[], int size) {
+    
+        void Insertionsort(int arr[], int size) {
         for(int i=1;i<size;i++){
             int key=arr[i];
+            cout<< "\nkey ="<< key;
             int j=i-1;
+            cout<<"\nj = " << j;
         
-        while(j>=0 &&arr[j]>key){
-            arr[j+1] =arr[i];
+        while(j>=0 && arr[j]>key){
+            arr[j+1] =arr[j];
+            cout <<"\narr[j+1] = "<< arr[j+1];  // value to compare with
             j--;
         }
         arr[j+1]=key;
+         cout << "\narr[j+1] = "<<arr[j+1]; 
         }
         }
-    };
+
 int main()
 {
     int size;
-    cout << "Enter the size of array ";
+    cout << "\nEnter the size of array ";
     cin >> size;
     int arr[size];
-    cout << "Enter the elements of array ";
+    cout << "\nEnter the elements of array ";
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
-     cout << "Original array is ";
+     cout << "\nOriginal array is ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
-    InsertionSort insertionSort;
-    insertionSort.sort(arr, size);
-    cout << "Sorted array is ";
+    
+    Insertionsort(arr, size);
+    cout << "\nSorted array is ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
