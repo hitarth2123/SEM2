54f1465b28a15aed5e5a9801f6dba52be05da73d
hitarth2123 <hitarthv2115@gmail.com>
2025-01-21 22:34:40 +0530

    Sprint1

---
+++ Sprint1/JAVA/Assignments/HitarthPatel_150096724046_21JAN2025_Java_Assignemnt_3/TwoStringCheck.java
@@ -0,0 +1,12 @@
+public class TwoStringCheck {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        if (str1.equals(str2)) {
+            System.out.println("Both strings contain the same data.");
+        } else {
+            System.out.println("Both strings do not contain the same data.");
+        }
+    }
+}
+
