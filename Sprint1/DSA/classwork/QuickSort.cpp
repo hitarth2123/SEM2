86b9eddc70be897f5aca0b2357ab85371fa97c43
hitarth2123 <hitarthv2115@gmail.com>
2025-01-23 10:28:48 +0530

    Sprint1

---
+++ Sprint1/DSA/classwork/QuickSort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    cout << "Original array is: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    quickSort(arr, 0, size - 1);
+    cout << "\nSorted array is: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
